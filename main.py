95804ba5fb23c3c334111a44135e6811188a3981
NarutoxHinata84529 <yornsama84@gmail.com>
2025-07-01 16:17:08 +0530

    Update main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+# Your manager bot token
+BOT_TOKEN = "7880080240:AAHUBpbJ58ZQ33uPekAsu2coyiGHMDTo_xc"
+
+bot = Client("manager_bot", bot_token=BOT_TOKEN)
+
+# /start command
+@bot.on_message(filters.command("start"))
+async def start(client, message: Message):
+    await message.reply_text(
+        "👋 Hello! I'm your Manager Bot.\n\n"
+        "Use /help to see what I can do."
+    )
+
+# /help command
+@bot.on_message(filters.command("help"))
+async def help_cmd(client, message: Message):
+    await message.reply_text(
+        "📖 <b>Available Commands:</b>\n\n"
+        "/start - Welcome message\n"
+        "/help - Show this help menu\n",
+        parse_mode="html"
+    )
+
+bot.run()
