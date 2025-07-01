101b745940af1568e9b704ff0130058bc0344091
NarutoxHinata84529 <yornsama84@gmail.com>
2025-07-01 16:24:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,34 @@
-from pyrogram import Client, filters
-from pyrogram.types import Message
+import telebot
 
 # Your manager bot token
 BOT_TOKEN = "7880080240:AAHUBpbJ58ZQ33uPekAsu2coyiGHMDTo_xc"
 
-bot = Client("manager_bot", bot_token=BOT_TOKEN)
+bot = telebot.TeleBot(BOT_TOKEN)
 
 # /start command
-@bot.on_message(filters.command("start"))
-async def start(client, message: Message):
-    await message.reply_text(
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message,
         "👋 Hello! I'm your Manager Bot.\n\n"
         "Use /help to see what I can do."
     )
 
 # /help command
-@bot.on_message(filters.command("help"))
-async def help_cmd(client, message: Message):
-    await message.reply_text(
-        "📖 <b>Available Commands:</b>\n\n"
+@bot.message_handler(commands=['help'])
+def send_help(message):
+    bot.reply_to(message,
+        "📖 *Available Commands:*\n\n"
         "/start - Welcome message\n"
-        "/help - Show this help menu\n",
-        parse_mode="html"
+        "/help - Show this help menu\n"
+        "/joke - Get a random joke\n"
+        "/kang - Steal a sticker/gif to your pack\n"
+        "/afk [reason] - Set yourself as AFK\n"
+        "/brb [reason] - Set BRB status\n"
+        "/insult - Send a funny insult\n"
+        "/promote - Promote a user\n"
+        "/fullpromote - Full admin rights\n"
+        "/demote - Demote a user\n",
+        parse_mode="Markdown"
     )
 
-bot.run()
+bot.infinity_polling()
